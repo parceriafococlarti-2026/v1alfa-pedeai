4e8349d5fad092ed5dd624bfe92eb7b4c1b3603f
Lucas Silva <parceriafococlarti@gmail.com>
2026-03-10 03:23:48 -0300

    Deploy para o Vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -31,6 +31,4 @@ app.use('/auth', authRoutes)
 const PORT = process.env.PORT || 3000
 
 // Sobe o servidor
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`)
-})
+module.exports = app
